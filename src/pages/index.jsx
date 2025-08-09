6fdd027355bf52399a60e05abf06eafef0d6a4c3
danielmoessner <github@danielmoessner.de>
2025-08-09 22:00:47 +0200

    neue texte

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,10 +7,10 @@ import Seo from '../components/Seo';
 
 function Patterns() {
   return (
-    <div className="absolute inset-y-0 h-full w-full" aria-hidden="true">
+    <div className="absolute inset-y-0 w-full h-full" aria-hidden="true">
       <div className="relative h-full">
         <svg
-          className="absolute right-full transform translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
+          className="absolute transform right-full translate-y-1/3 translate-x-1/4 md:translate-y-1/2 sm:translate-x-1/2 lg:translate-x-full"
           width="404"
           height="784"
           fill="none"
@@ -38,7 +38,7 @@ function Patterns() {
           <rect width="404" height="784" fill="url(#e229dbec-10e9-49ee-8ec3-0286ca089edf)" />
         </svg>
         <svg
-          className="absolute left-full transform -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
+          className="absolute transform left-full -translate-y-3/4 -translate-x-1/4 sm:-translate-x-1/2 md:-translate-y-1/2 lg:-translate-x-3/4"
           width="404"
           height="784"
           fill="none"
@@ -83,8 +83,7 @@ function Page({ data }) {
     }
     const script = doc.createElement('script');
     script.id = 'baufi-lead-script';
-    script.src =
-      'https://www.baufi-lead.de/baufilead/partner/dvvEderFNPvzhryAcDJwqVsqxpCjmC/imports.js';
+    script.src = 'https://baufi-passt.passt.aws.europace.de/baufi-passt-flex/baufi-passt-flex.js';
     script.defer = true;
     doc.body.append(script);
   }, []);
@@ -93,16 +92,16 @@ function Page({ data }) {
     <Layout>
       <Seo meta={page.meta} />
       <header className="bg-gray-50">
-        <div className="relative overflow-hidden pt-16">
+        <div className="relative pt-16 overflow-hidden">
           <Patterns />
-          <div className="pb-16 sm:pb-24 relative">
-            <div className="pt-16 mx-auto max-w-7xl px-4 sm:pt-24 sm:px-6">
+          <div className="relative pb-16 sm:pb-24">
+            <div className="px-4 pt-16 mx-auto max-w-7xl sm:pt-24 sm:px-6">
               <div className="text-center">
-                <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                   <span className="block">{page.header.title}</span>
                   <span className="block text-blue-600">{page.header.titleColor}</span>
                 </h1>
-                <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+                <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                   {page.header.text}
                 </p>
               </div>
@@ -113,19 +112,19 @@ function Page({ data }) {
               <div className="flex-1" />
               <div className="flex-1 w-full bg-gray-800" />
             </div>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6">
-              <div className="bg-white pt-6 pb-8 px-8 relative rounded-lg shadow-lg">
-                <h2 className="text-3xl tracking-tight font-bold text-gray-900">
+            <div className="px-4 mx-auto max-w-7xl sm:px-6">
+              <div className="relative px-8 pt-6 pb-8 bg-white rounded-lg shadow-lg">
+                <h2 className="text-3xl font-bold tracking-tight text-gray-900">
                   {page.form.title}
                 </h2>
-                <form className="flex flex-col space-y-6 mt-8 lg:flex-row lg:flex-nowrap lg:space-x-4 lg:space-y-0">
+                <form className="flex flex-col mt-8 space-y-6 lg:flex-row lg:flex-nowrap lg:space-x-4 lg:space-y-0">
                   <div className="lg:w-1/5">
                     <label htmlFor="grund" className="block text-sm font-medium text-gray-700">
                       {page.form.label1}
                       <select
                         id="grund"
                         defaultValue="KAUF"
-                        className="baufilead_finanzierungszweck text-gray-700 mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm rounded-md"
+                        className="block w-full py-2 pl-3 pr-10 mt-1 text-base text-gray-700 border-gray-300 rounded-md baufilead_finanzierungszweck focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                       >
                         <option value="KAUF">Immobilienkauf</option>
                         <option value="BAU">Bauvorhaben</option>
@@ -137,15 +136,15 @@ function Page({ data }) {
                   <div className="lg:w-1/5">
                     <label htmlFor="summe" className="block text-sm font-medium text-gray-700">
                       {page.form.label2}
-                      <div className="mt-1 relative rounded-md shadow-sm">
+                      <div className="relative mt-1 rounded-md shadow-sm">
                         <input
                           type="number"
                           id="summe"
                           step="10000"
-                          className="baufilead_darlehenssumme placeholder-gray-300 text-gray-700 focus:ring-blue-500 focus:border-blue-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
+                          className="block w-full pr-12 text-gray-700 placeholder-gray-300 border-gray-300 rounded-md baufilead_darlehenssumme focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                           placeholder="200000"
                         />
-                        <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                        <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                           <span className="text-gray-500 sm:text-sm">€</span>
                         </div>
                       </div>
@@ -154,15 +153,15 @@ function Page({ data }) {
                   <div className="lg:w-1/5">
                     <label htmlFor="kosten" className="block text-sm font-medium text-gray-700">
                       {page.form.label3}
-                      <div className="mt-1 relative rounded-md shadow-sm">
+                      <div className="relative mt-1 rounded-md shadow-sm">
                         <input
                           type="number"
                           id="kosten"
                           step="10000"
-                          className="baufilead_anschaffungskosten text-gray-700 placeholder-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
+                          className="block w-full pr-12 text-gray-700 placeholder-gray-300 border-gray-300 rounded-md baufilead_anschaffungskosten focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                           placeholder="100000"
                         />
-                        <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
+                        <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
                           <span className="text-gray-500 sm:text-sm">€</span>
                         </div>
                       </div>
@@ -175,7 +174,7 @@ function Page({ data }) {
                         <input
                           type="number"
                           id="ort"
-                          className="baufilead_postleitzahl shadow-sm placeholder-gray-300 focus:ring-blue-500 focus:border-blue-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                          className="block w-full placeholder-gray-300 border-gray-300 rounded-md shadow-sm baufilead_postleitzahl focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
                           placeholder="90933"
                         />
                       </div>
@@ -184,7 +183,7 @@ function Page({ data }) {
                   <button
                     type="submit"
                     onClick={(event) => event.preventDefault()}
-                    className="baufilead_konditionsrechner lg:w-1/5 self-end ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                    className="inline-flex self-end justify-center px-4 py-2 ml-3 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm baufilead_konditionsrechner lg:w-1/5 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                   >
                     {page.form.button}
                   </button>
@@ -194,13 +193,13 @@ function Page({ data }) {
           </div>
         </div>
         <div className="bg-gray-800">
-          <div className="max-w-7xl mx-auto py-16 px-4 sm:py-16 sm:px-6 lg:px-8" />
+          <div className="px-4 py-16 mx-auto max-w-7xl sm:py-16 sm:px-6 lg:px-8" />
         </div>
       </header>
 
       <section>
         <div className="bg-white">
-          <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:py-24 lg:px-8">
+          <div className="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:py-24 lg:px-8">
             <div className="max-w-3xl mx-auto text-center">
               <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
                 {page.offer.title}
@@ -211,7 +210,7 @@ function Page({ data }) {
               {page.offer.offers.map((offer) => (
                 <div className="flex" key={offer.link}>
                   <svg
-                    className="flex-shrink-0 h-6 w-6 text-blue-700"
+                    className="flex-shrink-0 w-6 h-6 text-blue-700"
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
                     viewBox="0 0 24 24"
@@ -226,7 +225,7 @@ function Page({ data }) {
                     />
                   </svg>
                   <div className="ml-3">
-                    <dt className="text-lg leading-6 font-medium text-gray-900">{offer.title}</dt>
+                    <dt className="text-lg font-medium leading-6 text-gray-900">{offer.title}</dt>
                     <dd className="mt-2 text-base text-gray-500">
                       {offer.text}
                       <div className="block mt-2">
@@ -247,8 +246,8 @@ function Page({ data }) {
       </section>
 
       <section>
-        <div className="bg-gray-50 pt-16 sm:pt-24">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="pt-16 bg-gray-50 sm:pt-24">
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="max-w-4xl mx-auto text-center">
               <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
                 {page.plan.title}
@@ -256,9 +255,9 @@ function Page({ data }) {
               <p className="mt-3 text-lg text-gray-500 sm:mt-4">{page.plan.text}</p>
             </div>
           </div>
-          <div className="mt-10 pb-16 sm:pb-28">
+          <div className="pb-16 mt-10 sm:pb-28">
             <div className="relative">
-              <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <div className="max-w-4xl mx-auto">
                   <dl
                     className={`rounded-lg bg-white shadow-lg divide-y-2 divide-gray-050 md:divide-y-0 md:divide-x-2 md:grid ${
@@ -280,18 +279,18 @@ function Page({ data }) {
       </section>
 
       <section className="bg-white">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
           <div className="pt-16 pb-20 lg:pt-24 lg:pb-28">
             <div className="relative divide-y-2 divide-gray-200 lg:max-w-7xl">
               <div>
-                <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">
+                <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                   {page.articles.title}
                 </h2>
                 <div className="mt-3 sm:mt-4 lg:grid lg:grid-cols-2 lg:gap-5 lg:items-center">
                   <p className="text-xl text-gray-500">{page.articles.text}</p>
                 </div>
               </div>
-              <div className="mt-6 pt-10 grid gap-16 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
+              <div className="grid gap-16 pt-10 mt-6 lg:grid-cols-2 lg:gap-x-5 lg:gap-y-12">
                 {articles.map((article) => (
                   <ArticleItem
                     key={article.slug}
@@ -307,17 +306,17 @@ function Page({ data }) {
       </section>
       <section>
         <div className="bg-gray-800">
-          <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+          <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
             <h2 className="text-2xl font-extrabold tracking-tight text-white sm:text-3xl">
               <span className="block">{page.cta.textTop}</span>
               <span className="block text-blue-200">{page.cta.textBottom}</span>
             </h2>
-            <div className="mt-8 flex lg:mt-0 lg:flex-shrink-0">
+            <div className="flex mt-8 lg:mt-0 lg:flex-shrink-0">
               <div className="inline-flex rounded-md shadow" />
-              <div className="ml-3 inline-flex rounded-md shadow">
+              <div className="inline-flex ml-3 rounded-md shadow">
                 <button
                   type="button"
-                  className="baufilead_konditionsrechner inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-600 ring-offset-gray-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  className="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm baufilead_konditionsrechner ring-offset-gray-800 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
                 >
                   {page.cta.button}
                 </button>
