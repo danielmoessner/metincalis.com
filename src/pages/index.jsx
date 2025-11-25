e432b07a6f96dc9b15acc7f23003572968963bad
danielmoessner <github@danielmoessner.de>
2025-11-25 16:05:15 +0100

    suchauftrag link

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -160,6 +160,21 @@ function Page({ data }) {
                     Berechnen
                   </button>
                 </form>
+                 <h2 className="mt-8 text-xl font-bold tracking-tight text-gray-900">
+                  Suchauftrag:
+                </h2>
+                <p className='block mt-2 text-sm font-medium text-gray-700'>
+                  Sie können hier auch einen Suchauftrag anlegen, damit Sie sofort informiert werden, wenn eine passende Immobilie in Ihrer Wunschregion verfügbar ist.
+                </p>
+                <div className="mt-4">
+                 <a
+                    href="https://meineimmosuche.de/metin-calis"
+                    target="_blank"
+                    className="inline-flex self-end justify-center px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md shadow-sm baufilead_konditionsrechner lg:w-1/5 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+                  >
+                    Suchauftrag erstellen
+                  </a>
+                </div>
               </div>
             </div>
           </div>
